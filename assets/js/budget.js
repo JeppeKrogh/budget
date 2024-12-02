cb90b6f4f4378809c06c10a3cc3310ed8159e536
JeppeKrogh <31619535+JeppeKrogh@users.noreply.github.com>
2024-12-02 07:15:12 +0100

    Update budget.js

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,13 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-  });
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const minimumBudget = 22104;
   const maximumSelf = 2500;
@@ -128,6 +118,16 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("minimumMubbi").innerText = minimumMubbi;
     document.getElementById("maximumSelf").innerText = maximumSelf;
   }
+  
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
 });
 /*
 CHANGELOG:
