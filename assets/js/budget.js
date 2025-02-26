d09c0e70283d2a70a207f6aa4d70c808c7f766fa
jeppekrogh <jeppejulius@hotmail.com>
2025-02-26 22:42:37 +0100

    Added means of saving for a house

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -3,24 +3,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const maximumSelf = 2000;
   const minimumFood = 4500;
   const minimumMubbi = 100;
+  const minimumBackup = 15000;
   const idaIncomeInput = document.getElementById("idaIncome");
   const jeppeIncomeInput = document.getElementById("jeppeIncome");
+  const currentBackupInput = document.getElementById("currentBackup");
   const calcButton = document.getElementById("calcButton");
 
   // Function to check if both input fields have values
   function checkInputs() {
     const idaIncomeValue = idaIncomeInput.value.trim();
     const jeppeIncomeValue = jeppeIncomeInput.value.trim();
+    const currentBackupValue = currentBackupInput.value.trim();
 
     // Enable the button if both input fields have values, otherwise disable it
-    calcButton.disabled = !(idaIncomeValue && jeppeIncomeValue);
+    calcButton.disabled = !(
+      idaIncomeValue &&
+      jeppeIncomeValue &&
+      currentBackupValue
+    );
   }
 
-  setPredefinedValues(minimumBudget, minimumFood, minimumMubbi, maximumSelf);
+  setPredefinedValues(
+    minimumBudget,
+    minimumFood,
+    minimumMubbi,
+    maximumSelf,
+    minimumBackup,
+  );
 
   // Add event listeners to the input fields
   idaIncomeInput.addEventListener("input", checkInputs);
   jeppeIncomeInput.addEventListener("input", checkInputs);
+  currentBackupInput.addEventListener("input", checkInputs);
   calcButton.addEventListener("click", calculateTransfers);
 
   function calculateTransfers() {
@@ -30,36 +44,38 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("jeppeIncome").value,
     );
     const totalBudget = parseFloat(idaIncome) + parseFloat(jeppeIncome);
+    const backupDiff =
+      parseFloat(minimumBackup) - parseFloat(currentBackupInput.value.trim());
 
     let idaBudget = Math.round((idaIncome / totalBudget) * minimumBudget);
-    let idaSaving = Math.round(idaIncome / totalBudget);
     let idaFood = Math.round((idaIncome / totalBudget) * minimumFood);
     let idaMubbi = Math.round((idaIncome / totalBudget) * minimumMubbi);
-    let idaSelf = Math.round(
-      idaIncome - idaBudget - idaSaving - idaFood - idaMubbi,
+    let idaBackup = Math.round((idaIncome / totalBudget) * backupDiff);
+    let idaLeft = Math.round(
+      idaIncome - idaBudget - idaFood - idaMubbi - idaBackup,
     );
 
-    if (idaSelf > maximumSelf) {
-      let amountAbove = idaSelf - maximumSelf;
-      idaSelf = idaSelf - amountAbove;
-      idaSaving = idaSaving + amountAbove;
-    }
+    let idaAmountAbove = idaLeft - maximumSelf;
+    idaLeft = idaLeft - idaAmountAbove;
+    idaHouse = idaAmountAbove;
 
     idaContainer.innerHTML = "";
 
     const idaBudgetHtml = createHtmlElement("Budget", idaBudget);
-    const idaSavingHtml = createHtmlElement("Opsparing", idaSaving);
+    const idaBackupHtml = createHtmlElement("Opsparing", idaBackup);
     const idaFoodHtml = createHtmlElement("Mad", idaFood);
     const idaMubbiHtml = createHtmlElement("Mubbi", idaMubbi);
-    const idaSelfHtml = createHtmlElement("Selv", idaSelf);
+    const idaHouseHtml = createHtmlElement("House", idaHouse);
+    const idaLeftHtml = createHtmlElement("Selv", idaLeft);
 
     const idaHtml = `
     <h3><b>👸</b></h3>
     ${idaBudgetHtml}
-    ${idaSavingHtml}
+    ${idaBackupHtml}
     ${idaFoodHtml}
     ${idaMubbiHtml}
-    ${idaSelfHtml}
+    ${idaHouseHtml}
+    ${idaLeftHtml}
   `;
 
     document.getElementById("idaContainer").innerHTML = idaHtml;
@@ -68,39 +84,40 @@ document.addEventListener("DOMContentLoaded", function () {
     let jeppeSaving = Math.round(jeppeIncome / totalBudget);
     let jeppeFood = Math.round((jeppeIncome / totalBudget) * minimumFood);
     let jeppeMubbi = Math.round((jeppeIncome / totalBudget) * minimumMubbi);
-    let jeppeSelf = Math.round(
+    let jeppeBackup = Math.round((jeppeIncome / totalBudget) * backupDiff);
+    let jeppeLeft = Math.round(
       jeppeIncome - jeppeBudget - jeppeSaving - jeppeFood - jeppeMubbi,
     );
 
-    if (jeppeSelf > maximumSelf) {
-      let amountAbove = jeppeSelf - maximumSelf;
-      jeppeSelf = jeppeSelf - amountAbove;
-      jeppeSaving = jeppeSaving + amountAbove;
-    }
+    let jeppeAmountAbove = jeppeLeft - maximumSelf;
+    jeppeLeft = jeppeLeft - jeppeAmountAbove;
+    jeppeHouse = jeppeAmountAbove;
 
     const jeppeBudgetHtml = createHtmlElement("Budget", jeppeBudget);
-    const jeppeSavingHtml = createHtmlElement("Opsparing", jeppeSaving);
+    const jeppeBackupHtml = createHtmlElement("Opsparing", jeppeBackup);
     const jeppeFoodHtml = createHtmlElement("Mad", jeppeFood);
     const jeppeMubbiHtml = createHtmlElement("Mubbi", jeppeMubbi);
-    const jeppeSelfHtml = createHtmlElement("Selv", jeppeSelf);
+    const jeppeHouseHtml = createHtmlElement("House", jeppeHouse);
+    const jeppeLeftHtml = createHtmlElement("Selv", jeppeLeft);
 
     const jeppeHtml = `
     <h3><b>🤴</b></h3>
     ${jeppeBudgetHtml}
-    ${jeppeSavingHtml}
+    ${jeppeBackupHtml}
     ${jeppeFoodHtml}
     ${jeppeMubbiHtml}
-    ${jeppeSelfHtml}
+    ${jeppeHouseHtml}
+    ${jeppeLeftHtml}
   `;
 
     document.getElementById("jeppeContainer").innerHTML = jeppeHtml;
 
-    let totalSaving = idaSaving + jeppeSaving;
-    const totalSavingHtml = createHtmlElement("Opsparing", totalSaving);
+    let totalHouse = idaHouse + jeppeHouse;
+    const totalHouseHtml = createHtmlElement("Hus", totalHouse);
 
     const totalHtml = `
     <h3><b>Samlet</b></h3>
-    ${totalSavingHtml}
+    ${totalHouseHtml}
     `;
     document.getElementById("totalContainer").innerHTML = totalHtml;
   }
@@ -112,11 +129,13 @@ document.addEventListener("DOMContentLoaded", function () {
     minimumFood,
     minimumMubbi,
     maximumSelf,
+    minimumBackup,
   ) {
     document.getElementById("minimumBudget").innerText = minimumBudget;
     document.getElementById("minimumFood").innerText = minimumFood;
     document.getElementById("minimumMubbi").innerText = minimumMubbi;
     document.getElementById("maximumSelf").innerText = maximumSelf;
+    document.getElementById("minimumBackup").innerText = minimumBackup;
   }
 });
 /*
