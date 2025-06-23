3472e78974f6248c7ae13cb898271d0f0518e30a
Jeppe Krogh <kjej@aarhus.dk>
2025-06-23 12:22:48 +0200

    Upped minimumBudget to 25k

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const minimumBudget = 22104;
+  const minimumBudget = 25000;
   const maximumSelf = 2000;
   const minimumFood = 4500;
   const minimumMubbi = 100;
